b1155097c8a575d2387f2015e106abb1c91b0bfc
asafonov <me@asafonov.org>
2020-10-22 07:50:45 +0000

    hiding pass on generate

--- view/itemlist.js
+++ view/itemlist.js
@@ -73,6 +73,7 @@ class ItemListView {
   onGenerate() {
     this.model.update();
     this.element.querySelector('.generate .done').classList.add('true');
+    this.hidePass();
     setTimeout(this.hideAllDonesProxy, 2000);
   }
 
