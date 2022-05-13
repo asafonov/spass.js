631c72106e25b0224215e8e1b9bede30125ee8be
asafonov <me@asafonov.org>
2022-05-13 14:39:29 +0300

    fix copy to clipboard function on ios devices

--- view/itemlist.js
+++ view/itemlist.js
@@ -81,14 +81,33 @@ class ItemListView {
   }
 
   copyPasword() {
-    navigator.clipboard.writeText(this.model.get()).then(() => {
-      console.log("success");
-    }).catch((e)=> {
-      console.log(e);
-    });
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(this.model.get()).then(() => {
+        console.log("success");
+      }).catch((e)=> {
+        console.log(e);
+      });
+    } else { //Apple=Shit
+      const input = document.createElement('input')
+      input.contentEditable = true
+      input.readOnly = false
+      input.value = this.model.get()
+      this.element.appendChild(input)
+      const range = document.createRange()
+      range.selectNodeContents(input)
+      const selection = window.getSelection()
+      selection.removeAllRanges()
+      selection.addRange(range)
+      input.setSelectionRange(0, 99)
+      document.execCommand('copy')
+      this.element.removeChild(input)
+    }
 
-    if (NativeAndroid !== null && NativeAndroid !== undefined) {
-      NativeAndroid.copyToClipboard(this.model.get());
+    try {
+      if (NativeAndroid !== null && NativeAndroid !== undefined) {
+        NativeAndroid.copyToClipboard(this.model.get());
+      }
+    } catch (e) {
     }
   }
 
