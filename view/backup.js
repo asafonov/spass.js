326c75f61ee44a15411f3ab47da618b7f265400a
asafonov <me@asafonov.org>
2025-12-10 16:24:22 +0300

    generate qrcode for export

--- view/backup.js
+++ view/backup.js
@@ -4,9 +4,11 @@ class BackupView {
     this.list = list
     this.a = this.createDownloadElement()
     this.fileInput = this.createUploadElement()
+    this.qrCodeElement = this.createQRCodeElement()
+    this.qrCode = this.createQRCode()
     this.onPopupProxy = this.onPopup.bind(this)
-    this.onFileExportProxy = this.onFileExport.bind(this)
-    this.onFileImportProxy = this.onFileImport.bind(this)
+    this.onExportProxy = this.onExport.bind(this)
+    this.onImportProxy = this.onImport.bind(this)
     this.onFileUploadProxy = this.onFileUpload.bind(this)
     this.manageEventListeners()
   }
@@ -14,13 +16,47 @@ class BackupView {
   manageEventListeners (remove) {
     const action = remove ? 'removeEventListener' : 'addEventListener'
     this.element[action]('click', this.onPopupProxy)
-    this.element.querySelector('.file_up')[action]('click', this.onFileExportProxy)
-    this.element.querySelector('.file_down')[action]('click', this.onFileImportProxy)
+    this.element.querySelector('.file_up')[action]('click', this.onExportProxy)
+    this.element.querySelector('.file_down')[action]('click', this.onImportProxy)
     this.fileInput[action]('change', this.onFileUploadProxy)
   }
 
   onPopup() {
     this.element.classList.add('open')
+  }
+
+  createQRCode() {
+    const size = Math.min(document.documentElement.clientWidth, document.documentElement.clientHeight) * 0.9
+    return new QRCode(this.qrCodeElement.querySelector('div'), {width: size, height: size})
+  }
+
+  createQRCodeElement() {
+    const div = document.createElement('div')
+    const w = document.documentElement.clientWidth
+    const h = document.documentElement.clientHeight
+    div.style.position = 'fixed'
+    div.style.top = '0px'
+    div.style.left = '0px'
+    div.style.width = `${w}px`
+    div.style.height = `${h}px`
+    div.style.display = 'none'
+    div.style.zIndex = '999'
+    div.style.backgroundColor = '#ffffff'
+    div.style.justifyContent = 'center'
+    div.style.alignItems = 'center'
+    div.style.flexDirection = 'column'
+    document.body.appendChild(div)
+    const qrDiv = document.createElement('div')
+    div.appendChild(qrDiv)
+    const button = document.createElement('button')
+    button.innerHTML = 'Close'
+    button.value = 'Close'
+    button.style.marginTop = '20px'
+    button.addEventListener('click', () => {
+      div.style.display = 'none'
+    })
+    div.appendChild(button)
+    return div
   }
 
   createDownloadElement() {
@@ -39,13 +75,13 @@ class BackupView {
     return i
   }
 
-  onFileExport() {
-    this.a.href = URL.createObjectURL(new Blob([this.list.asString()], {type: 'text/json'}))
-    this.a.click()
+  onExport() {
+    this.qrCodeElement.style.display = 'flex'
+    this.qrCode.makeCode(this.list.asString())
     this.hidePopup()
   }
 
-  onFileImport() {
+  onImport() {
     this.fileInput.click()
   }
 
