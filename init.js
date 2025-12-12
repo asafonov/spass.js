601c7ffd0f5961a8bf44bb7af8ff4fc73ea6c8a8
asafonov <me@asafonov.org>
2025-12-12 14:20:11 +0300

    Revert "add telegram web-app script"
    This reverts commit 62658df2e76b4a40cb345c607c95f040b8d74513.

--- init.js
+++ init.js
@@ -1,13 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  if (window.Telegram && window.Telegram.WebApp) {
-    Telegram.WebApp.init({
-      onError: error => alert(error)
-    })
-
-    if (Telegram.WebApp.isCameraAccessAvailable()) {
-      Telegram.WebApp.requestCameraAccess(allowed => alert(allowed))
-    }
-  }
   const list = new List();
   const listView = new ListView(list);
   listView.render();
