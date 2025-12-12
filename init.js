62658df2e76b4a40cb345c607c95f040b8d74513
asafonov <me@asafonov.org>
2025-12-12 14:07:02 +0300

    add telegram web-app script

--- init.js
+++ init.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+  if (window.Telegram && window.Telegram.WebApp) {
+    Telegram.WebApp.init({
+      onError: error => alert(error)
+    })
+
+    if (Telegram.WebApp.isCameraAccessAvailable()) {
+      Telegram.WebApp.requestCameraAccess(allowed => alert(allowed))
+    }
+  }
   const list = new List();
   const listView = new ListView(list);
   listView.render();
